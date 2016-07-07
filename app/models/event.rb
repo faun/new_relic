794904c1cc1920eb2605e4cb975d22264d6f05a7
Faun <github@faun.me>
2016-07-07 10:08:19 -0700

    Add recent lookup

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ActiveRecord::Base
   belongs_to :host
   belongs_to :organization
+  validates_associated :organization
+  validates :organization, presence: true
+
+  def self.recent(count, org_id)
+    limit = Integer(count)
+    org = Integer(org_id)
+    where(organization_id: org).order('timestamp ASC').limit(limit)
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -8,4 +8,59 @@ RSpec.describe Event, type: :model do
       expect(event.payload).to eq 'hello'
     end
   end
+
+  describe '.recent' do
+    let(:org1) { Organization.create!(name: 'New Relic') }
+    let(:org2) { Organization.create!(name: 'Old Relic') }
+
+    let(:event1) do
+      Event.create!(
+        organization: org1,
+        payload: 'status ok',
+        timestamp: Time.now,
+        hostname: 'server 1'
+      )
+    end
+
+    let(:event2) do
+      Event.create!(
+        organization: org2,
+        payload: 'danger',
+        timestamp: Time.now,
+        hostname: 'server 2'
+      )
+    end
+
+    let(:event3) do
+      Event.create!(
+        organization: org1,
+        payload: 'status ok',
+        timestamp: Time.now,
+        hostname: 'server 3'
+      )
+    end
+
+    let(:event4) do
+      Event.create!(
+        organization: org1,
+        payload: 'status ok',
+        timestamp: Time.now,
+        hostname: 'server 3'
+      )
+    end
+
+    it 'returns events for the organization only' do
+      expect(
+        described_class.recent(3, org1.id)
+      ).to match_array(
+        [event1, event3, event4]
+      )
+
+      expect(
+        described_class.recent(3, org2.id)
+      ).to match_array(
+        [event2]
+      )
+    end
+  end
 end
